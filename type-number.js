183d8822dd32a09e950f24288c3d1ca8a313e819
João Colla <160884809+JoaoColla@users.noreply.github.com>
2025-05-10 10:02:08 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,26 @@
+//Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+// + adição _ - subtração _ * multiplicação _ / divisão
+
+console.log(operacaoMatematica)
+
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NAN -> Not a Number
+
+const alura = "alura";
+console.log(alura - primeiroNumero)
---
+++ type-string.js
@@ -0,0 +1,13 @@
+//type-sting - texto
+const texto1 = 'Olá Mundo!';
+const tet2 = "Olá Mundo!";
+const senha = "senhaSuperSegura456!"
+const strinDeNumero = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "João Colla"
+
+//conscatenação (+)
+console.log(citacao + meuNome)
+
+//template string ou template literal
