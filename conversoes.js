eea58565e2736a900a9fbac67605349589080ac3
João Colla <160884809+JoaoColla@users.noreply.github.com>
2025-05-11 18:12:33 -0300

    Add files via upload

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+//tipo de dado
+//boobleanos
+
+//conversão implicita
+const numero = 456;
+const numeroString = '456';
+
+
+//conversão explicita
+//Number()
+console.log(numero + Number(numeroString))
+
+//String()
+console.log(String(numero) + numeroString)
+
---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+//Boolean
+const usuarioLogado = true
+const contaPaga = false
+
+//truthy ou falsy
+// 0 => false
+// 1 => true
+
+// console.log(0 == false)
+// console.log('' == false)
+// console.log(1 == true)
+
+let minhaVar;
+let varNull = null;
+
+// console.log(typeof minhaVar)
+// console.log(typeof varNull)
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof numero)
+console.log(typeof texto)
