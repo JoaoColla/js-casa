6672befcfc913e985e92228da40154cd79a58282
João Colla <160884809+JoaoColla@users.noreply.github.com>
2025-05-18 10:26:05 -0300

    Add files via upload

---
+++ comparacoes.js
@@ -0,0 +1,18 @@
+// == (comparação implicita)
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto) //true
+console.log(numero === texto) //false
+
+//typeof
+console.log(typeof numero) 
+console.log(typeof texto) 
+
+// == so compara o valor
+// === compara o valor e o tipo de dado
+
+//conversão explicita
+
+Number()
+String()
